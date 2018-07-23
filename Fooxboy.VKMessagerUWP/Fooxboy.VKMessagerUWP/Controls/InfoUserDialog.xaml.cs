e9559b51bf511d63af53a9fd8b6bacaa50a34aa5
Славик Лис <Fooxboy@yandex.ru>
2018-07-23 19:33:28 +0300

    Начата работа над окном информации о пользователе

---
+++ Fooxboy.VKMessagerUWP/Fooxboy.VKMessagerUWP/Controls/InfoUserDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Fooxboy.VKMessagerUWP.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Диалоговое окно содержимого" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Fooxboy.VKMessagerUWP.Controls
+{
+    public sealed partial class InfoUserDialog : ContentDialog
+    {
+        public InfoUserDialogViewModel VM { get; set; }
+        public long UserId { get; set; }
+
+        public InfoUserDialog()
+        {
+            this.InitializeComponent();
+            VM = new InfoUserDialogViewModel(UserId);
+        }
+    }
+}
---
+++ Fooxboy.VKMessagerUWP/Fooxboy.VKMessagerUWP/ViewModel/InfoUserDialogViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Fooxboy.VKMessagerUWP.ViewModel
+{
+    public class InfoUserDialogViewModel :BaseViewModel
+    {
+        public long UserId;
+
+        public InfoUserDialogViewModel(long id)
+        {
+            UserId = id;
+        }
+
+        private Uri photo;
+        public Uri Photo
+        {
+            get => photo;
+            set
+            {
+                if (value == photo) return;
+
+                photo = value;
+                Changed("Photo");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (value == name) return;
+                name = value;
+                Changed("Name");
+            }
+        }
+
+        bool isLoading;
+        public bool IsLoading
+        {
+            get => isLoading;
+            set
+            {
+                if (value = isLoading) return;
+
+                isLoading = value;
+                Changed("IsLoading");
+            }
+        }
+
+        Visibility visibilityLoadingGrid;
+        public Visibility VisibilityLoadingGrid
+        {
+            get => visibilityLoadingGrid;
+            set
+            {
+                if (value == visibilityLoadingGrid) return;
+
+                visibilityLoadingGrid = value;
+                Changed("VisibilityLoadingGrid");
+            }
+        }
+
+
+        Visibility visibilityInfoGrid;
+        public Visibility VisibilityInfoGrid
+        {
+            get => visibilityInfoGrid;
+            set
+            {
+                if (value == visibilityInfoGrid) return;
+
+                visibilityInfoGrid = value;
+                Changed("VisibilityInfoGrid");
+            }
+        }
+    }
+}
